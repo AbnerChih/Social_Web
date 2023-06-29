b00a958599183e018ac3af653fc36c36077370f7
AbnerChih <wanghch841224@gmail.com>
2023-06-29 22:52:18 +0800

    New_Web

--- src/routes.js
+++ src/routes.js
@@ -4,8 +4,4 @@ import blogPost from './components/blogPost.vue';
 import addBlog from "./components/addBlog.vue";
 
 
-export default [
-  { path: "/", component: homePage },
-  { path: "/blogs", component: blogPost},
-  { path: "/addBlog", component: addBlog }
 ];
